bcfa39af4631a9c8b71ab2b59dbd4c4efe953c3a
jcantero <h1213579@mvrht.com>
2020-01-22 11:05:49 +0100

    crear CapaNegocio

---
+++ SerONoSer/CapaNegocio/Negocio.cs
@@ -0,0 +1,22 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaNegocio
+{
+    class Negocio
+    {
+        //Instanciamos el objeto gestor para poder usar los metodos de Datos.Gestor
+        public static CapaDatos.DatosDSet gestor = new CapaDatos.DatosDSet();
+
+        public List<Pregunta> PreguntasPorNivel(int nivel)
+        {
+            List<Pregunta> reguntas;
+            reguntas = gestor.PreguntasPorNivel(nivel);
+            return reguntas;
+        }
+    }
+}
